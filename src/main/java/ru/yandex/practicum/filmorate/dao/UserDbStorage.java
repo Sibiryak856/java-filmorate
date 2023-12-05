c8f0a73acecb82ba3b2a663d9864ce44e7361515
sibiryak856 <belyakov-semyon@yandex.ru>
2023-12-06 00:01:37 +0300

    database users 1/2 complete update getAll, friends

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -2,22 +2,18 @@ package ru.yandex.practicum.filmorate.dao;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.model.UserFriend;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+import static ru.yandex.practicum.filmorate.model.FriendshipStatus.APPROVED;
 
 @Component("userDb")
 //@Repository("userDb")
@@ -28,18 +24,9 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public List<User> getAll() {
-        List<User> users = new ArrayList<>();
-        SqlRowSet userRows = jdbcTemplate.queryForRowSet("select * from USERS");
-        while (userRows.next()) {
-            users.add(User.builder()
-                    .id(userRows.getLong("USER_ID"))
-                    .email(userRows.getString("EMAIL"))
-                    .login(userRows.getString("LOGIN"))
-                    .name(userRows.getString("USER_NAME"))
-                    .birthday(LocalDate.parse(userRows.getString("BIRTHDAY")))
-                    .build());
-        }
-        return users;
+        String sql = "select * from USERS";
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> makeUser(rs));
     }
 
     @Override
@@ -55,7 +42,7 @@ public class UserDbStorage implements UserStorage {
                 .email(userRows.getString("EMAIL"))
                 .login(userRows.getString("LOGIN"))
                 .name(userRows.getString("USER_NAME"))
-                .birthday(LocalDate.parse(userRows.getString("BIRTHDAY")))
+                .birthday(userRows.getDate("BIRTHDAY").toLocalDate())
                 .build());
     }
 
@@ -103,16 +90,45 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public List<User> getFriends(Long id) {
-        return null;
+        String sql = "select USER_ID, EMAIL, LOGIN, USER_NAME, BIRTHDAY from USERS u " +
+                "join USER_FRIENDS uf on u.USER_ID = uf.USER_ID " +
+                "group by uf.USER_ID " +
+                "where uf.USER_ID = ?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> makeUser(rs), id);
     }
 
     @Override
     public void addFriend(Long id, Long otherId) {
 
+
+        UserFriend userFriend = UserFriend.builder().
+                userId(id).
+                friendId(otherId).
+                build();
+        UserFriend addedUserFriend = UserFriend.builder().
+                userId(otherId).
+                friendId(id).
+                build();
+            String sqlQuery = "insert into USER_FRIENDS(USER_ID, FRIEND_ID, FRIEND_STATUS) " +
+                    "values (?, ?, ?)";
+            jdbcTemplate.update(sqlQuery,
+                    id,
+                    otherId,
+                    APPROVED.getValue());
     }
 
     @Override
     public void removeFriend(Long id, Long otherId) {
 
+    }
+
+    private User makeUser(ResultSet rs) throws SQLException {
+        return User.builder()
+                .id(rs.getLong("USER_ID"))
+                .email(rs.getString("EMAIL"))
+                .login(rs.getString("LOGIN"))
+                .name(rs.getString("USER_NAME"))
+                .birthday(rs.getDate("BIRTHDAY").toLocalDate())
+                .build();
     }
 }
